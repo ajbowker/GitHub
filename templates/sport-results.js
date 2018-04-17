5a679581eb73fe389f7c72891bda71a8e635252c
Andrew Leggett <me@andyleggett.co.uk>
2018-04-17 17:40:24 +0100

    Added new states, parsing row data from Big Ted to account for errors in input and empty rows and subheadings

--- templates/sport-results.js
+++ templates/sport-results.js
@@ -3,77 +3,92 @@ var headingHeight = 96;
 var rowHeight = 52;
 var subheadingHeight = 52;
 var footerHeight = 48;
-var hdDimensions = {width:1920, height:1080};
+var hdDimensions = {
+  width: 1920,
+  height: 1080
+};
 
 //dynamic height of classified board
-var rowsHeight = 0;
 var sliderHeight;
 var sliderPosition;
+
+//current state of display
+var currentState = 'out';
+var currentRawRows = '';
+var currentRowData;
+var currentTitle = '';
+var currentLogoId = '';
 
 //select elements from html
 var slider = document.querySelector('.js-slider');
 var board = document.querySelector('.js-board');
 var rows = document.querySelector('.js-data-rows');
 var heading = document.querySelector('.js-heading');
+var headingText = document.querySelector('.js-heading-text');
 var competitionLogo = document.querySelector('.js-competition-logo');
 
+//update function used by Caspar at window.update
+var update = function (message) {
 
-//update function used by Caspar at window.update to 
-var update = function (updatejson) {
+  var update = JSON.parse(message);
 
-  var update = JSON.parse(updatejson);
-
-  //what message do we have?
   switch (update.type) {
-    case 'setup':
+    case 'SPORT_RESULTS':
 
-      console.log('Creating the classified board with ' + update.rows);
+      switch (currentState) {
+        case 'out':
+          currentRowData = parseRows(update.rows);
+          currentRawRows = update.rows;
 
-      //create the rows of the classified board updating dimensions as we go
-      var rowData = update.rows.split('#');
-      rowsHeight = 0;
-      
-      for (var i = 0; i < rowData.length; i += 1) {
-        var row = rowData[i];
-        rows.appendChild(createRow(row, i));
-        rowsHeight += rowHeight;
+          rows.innerHTML = '';
+
+          for (var i = 0; i < currentRowData.length; i += 1) {
+            rows.appendChild(createRow(currentRowData[i]));
+          }
+
+          //set the title and competition sponsor logo
+          currentTitle = update.title;
+          headingText.innerText = update.title;
+
+          currentLogoId = update.logoId;
+          competitionLogo.src = 'img/competition_logos/' + update.logoId + '.png';
+
+          //position the slider
+          sliderHeight = ((rowHeight * currentRowData.length) + headingHeight + footerHeight);
+          sliderPosition = (hdDimensions.height - sliderHeight) / 2;
+
+          slider.style.height = sliderHeight + 'px';
+          slider.style.webkitTransform = 'translateX(-450px) translateY(' + sliderPosition + 'px)';
+
+          //expand the classified board
+          window.requestAnimationFrame(function () {
+            window.requestAnimationFrame(function () {
+
+              addClass(slider, 'expander');
+              slider.style.webkitTransform = 'translateX(221px) translateY(' + sliderPosition + 'px)';
+              slider.style.width = '1369px';
+
+              rows.style.height = (rowHeight * currentRowData.length) + 'px';
+
+              addClass(heading, 'expand');
+              addClass(competitionLogo, 'expand');
+
+              currentState = 'in';
+            });
+          });
+
+          break;
+
+        case 'in':
+
+
+          break;
+
       }
-
-      sliderHeight = (rowsHeight + headingHeight + subheadingHeight + footerHeight);
-      sliderPosition = (hdDimensions.height - sliderHeight)/2;
-
-      slider.style.height = sliderHeight + 'px';
-      slider.style.webkitTransform = 'translateX(-450px) translateY(' + sliderPosition + 'px)';
 
       break;
 
-    case 'animate':
-
-      console.log('Animating to state - ' + update.state);
-
-      //each of the animate "states" removes css styles and classes
-      if (update.state === 'expand') {
-
-        //expand the classified board
-        slider.style.webkitTransform = 'translateX(221px) translateY(' + sliderPosition + 'px)';
-        slider.style.width = '1369px';
-
-        rows.style.height = (rowsHeight + subheadingHeight) + 'px';
-
-        addClass(heading, 'expand');
-        addClass(competitionLogo, 'expand');
-
-      } else if (update.state === 'shrink') {
-
-        //shrink the classified board
-        slider.style.webkitTransform = 'translateX(-450px) translateY(' + sliderPosition + 'px)';
-        slider.style.width = '0px';
-
-        rows.style.height = '0px';
-
-        removeClass(heading, 'expand');
-        removeClass(competitionLogo, 'expand');
-      }
+    case 'SPORT_RESULTS_OUT':
 
       break;
   }
@@ -88,18 +103,84 @@ var removeClass = function (element, classname) {
   element.classList.remove(classname);
 };
 
-var createRow = function (rowData, index) {
-  var items = rowData.split(',');
+var parseRows = function (rowdata) {
+  var rows = rowdata.split('#');
+  var parsedRows = [];
+  var items;
+  var trimmedItem;
+  var trimmedItems;
+  var matchCounter = 0;
 
+  for (var i = 0; i < rows.length; i += 1) {
+    var items = rows[i].split('~');
+    trimmedItems = [];
+
+    for (var j = 0; j < items.length; j += 1) {
+      trimmedItem = items[j].trim();
+
+      if (trimmedItem !== '') {
+        trimmedItems.push(trimmedItem);
+      }
+    }
+
+    if (trimmedItems.length === 0) {
+      //do nothing with empty row
+    } else if (trimmedItems.length === 1) {
+      parsedRows.push({
+        type: 'subheading',
+        text: trimmedItems[0]
+      })
+    } else if (trimmedItems.length > 1) {
+      var scoreMatches = trimmedItems[1].match(/\d+/g);
+
+      if (scoreMatches !== null && scoreMatches.length === 2) {
+        //has two scores
+        parsedRows.push({
+          type: 'score',
+          colour: (matchCounter % 2) ? 'grey' : 'white',
+          homeTeam: trimmedItems[0].toUpperCase(),
+          betweenText: scoreMatches[0] + ' - ' + scoreMatches[1],
+          betweenType: 'scores',
+          awayTeam: trimmedItems[2].toUpperCase()
+        })
+      } else {
+        //is some other text
+        parsedRows.push({
+          type: 'score',
+          colour: (matchCounter % 2) ? 'white' : 'grey',
+          homeTeam: trimmedItems[0].toUpperCase(),
+          betweenText: trimmedItems[1].toUpperCase(),
+          betweenType: 'other',
+          awayTeam: trimmedItems[2].toUpperCase()
+        })
+      }
+
+      matchCounter += 1;
+    }
+  }
+
+  return parsedRows;
+};
+
+var createRow = function (row) {
   var newRow = document.createElement('li');
-  addClass(newRow, 'row');
-  addClass(newRow, (index % 2 === 0) ? 'white' : 'grey');
 
-  var scoreline = '<div class="home-team">' + items[0] + '</div>';
-  scoreline += '<div class="score">' + items[1] + ' - ' + items[2] + '</div>';
-  scoreline += '<div class="away-team">' + items[3] + '</div>';
+  if (row.type === 'subheading') {
+    //subheading
+    addClass(newRow, 'row');
+    addClass(newRow, 'subheading');
 
-  newRow.innerHTML = scoreline;
+    newRow.innerHTML = '<h2>' + row.text + '</h2>';
+  } else if (row.type === 'score') {
+    addClass(newRow, 'row');
+    addClass(newRow, row.colour);
+
+    var scoreline = '<div class="home-team">' + row.homeTeam + '</div>';
+    scoreline += '<div class="between ' + row.betweenType + '">' + row.betweenText + '</div>';
+    scoreline += '<div class="away-team">' + row.awayTeam + '</div>';
+
+    newRow.innerHTML = scoreline;
+  }
 
   return newRow;
 }
---
+++ templates/test.js
@@ -0,0 +1,7 @@
+update('{"type":"SPORT_RESULTS","title":"LADBROKES SCOTTISH PREMIERSHIP","logoId":"1","rows":"Saturday~~#CELTIC~1-2~RANGERS#HEART OF MIDLOTHIAN~3-4~HIBERNIAN#Sunday#ST JOHNSTON~0-5~ABERDEEN#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~"}')
+
+update('{"type":"SPORT_RESULTS","title":"LADBROKES SCOTTISH CHAMPIONSHIP","logoId":"2","rows":"Fixtures~~#DUNDEE UTD~1-0~FALKIRK#DUNFERMLINE ATHLETIC~4-0~BRECHIN CITY#INVERNESS CT~5-1~DUMBARTON#MORTON~P-P~QUEEN OF THE SOUTH#ST MIRREN~0-0~LIVINGSTON#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~"}')
+
+update('{"type":"SPORT_RESULTS","title":"WILLIAM HILL SCOTTISH CUP","logoId":"5","rows":"This Weekend~~#MOTHERWELL~3-0~ABERDEEN#RANGERS~SUN~#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~#~~~"}')
+
+update('{"type":"SPORT_RESULTS_OFF"}')
